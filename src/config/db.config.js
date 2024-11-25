920214f2e15176778dae7772e9eb14091155eafe
smdekate <smdekate@gmail.com>
2024-11-25 13:22:05 +0530

    database connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,17 @@
 // Database configuration will go here
-export const dbConfig = {
-    // MongoDB configuration will be added here
+import mongoose from "mongoose";
+import { ApiError } from "../utils/ApiError.js";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\nMongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MONGODB connection FAILED: ", error);
+        throw new ApiError(500, "Failed to connect to database");
+        process.exit(1);
+    }
 };
+
+export default connectDB;
